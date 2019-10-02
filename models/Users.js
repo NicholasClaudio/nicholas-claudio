7e0173f357b827057a950aff73b904572952f33e
Nicholas Claudio <nclaudio449@gmail.com>
2019-10-02 18:54:40 -0500

    Added user registration endpoint

--- models/Users.js
+++ models/Users.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.SchemaType( {
   name: {
     type: String,
-    required: true;
+    required: true
   },
   email: {
     type: String,
